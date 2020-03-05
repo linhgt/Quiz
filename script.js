98767ee68e66bdaf9e9fa043da0a85c7e705f3cc
Linh Nguyen <linh.nguyen2@wsu.edu>
2020-03-04 19:13:39 -0800

    Basic script for the quiz

---
+++ script.js
@@ -0,0 +1,19 @@
+(function(){
+    //Container for the quiz, result and the submit button
+    const quizContainer = document.getElementById("quiz");
+    const resultContainer = document.getElementById("result");
+    const submitButton = document.getElementById("submit");
+
+    function buildQuiz(){
+
+    }
+
+    function showResult(){
+
+    }
+
+    buildQuiz();
+
+    //on submit, display result
+    submitButton.addEventListener("click", showResult);
+})();
