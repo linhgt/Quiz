c042afb29b2a947914502135de7d3d6699a300ab
Linh Nguyen <linh.nguyen2@wsu.edu>
2020-03-06 22:44:28 -0800

    Fix the error that the quiz isn't built succesfully

--- script.js
+++ script.js
@@ -43,29 +43,68 @@
         const output=[];
 
         //For each questions
-        Questions.forEach = (currentQuestion, questionNumber) => {
+        Questions.forEach(
+            (currentQuestion, questionNumber) => {
             //Store the list of answer choices
             const answers = [];
 
             //For each answer
             for (letter in currentQuestion.answers)
             {
+                //Add an html button
                 answers.push(
-                    //Add an html button
                     `<label>
                         <input type="radio" name="question${questionNumber}" value="${letter}">
                             ${letter} :
-                            ${currentQuestions.answers[letter]}
+                            ${currentQuestion.answers[letter]}
                         </input>
                     </label>`
                 ); 
             }
             output.push(
-                `<div class="questions">${currentQuestion.question}</div>
+                `<div class="question">${currentQuestion.question}</div>
                 <div class="answers">${answers.join('')}</div>`
             );
         }
+        );
+        // finally combine our output list into one string of HTML and put it on the page
+        quizContainer.innerHTML = output.join('');
     }
+    /*function buildQuiz(){
+        // variable to store the HTML output
+        const output = [];
+    
+        // for each question...
+        Questions.forEach(
+          (currentQuestion, questionNumber) => {
+    
+            // variable to store the list of possible answers
+            const answers = [];
+    
+            // and for each available answer...
+            for(letter in currentQuestion.answers){
+    
+              // ...add an HTML radio button
+              answers.push(
+                `<label>
+                  <input type="radio" name="question${questionNumber}" value="${letter}">
+                  ${letter} :
+                  ${currentQuestion.answers[letter]}
+                </label>`
+              );
+            }
+    
+            // add this question and its answers to the output
+            output.push(
+              `<div class="question"> ${currentQuestion.question} </div>
+              <div class="answers"> ${answers.join('')} </div>`
+            );
+          }
+        );
+    
+        // finally combine our output list into one string of HTML and put it on the page
+        quizContainer.innerHTML = output.join('');
+      }*/
     function showResult(){
         // gather answer containers from our quiz
         const answerContainers = quizContainer.querySelectorAll('.answers');
